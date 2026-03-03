ce0490f75404f2d40df07d26c5c3f6171c35ed14
Amit pant <139322726+pantamit2003@users.noreply.github.com>
2026-03-03 11:17:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -252,6 +252,13 @@ if st.session_state.logged and not st.session_state.admin:
     already_in = (today_df["punch_type"] == "IN").any()
     already_out = (today_df["punch_type"] == "OUT").any()
 
+    st.write("User Clean:", user_clean)
+    st.write("Today:", today)
+    st.write("Filtered Data:")
+    st.dataframe(today_df)
+    st.write("already_in:", already_in)
+    st.write("already_out:", already_out)
+
     st.write("already_in:", already_in)
     st.write("already_out:", already_out)
     st.write("today:", today)
@@ -420,6 +427,7 @@ if st.session_state.logged:
         st.session_state.clear()
         st.query_params.clear()
         st.rerun()
+
 
 
 
